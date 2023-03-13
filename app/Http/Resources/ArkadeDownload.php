e9e1fe32e0d4eef6e66abf46ec4ce882cf3a1287
Jørgen Tellnes <jorgen@arkitektum.no>
2023-03-13 18:16:24 +0100

    Add API resource classes

---
+++ app/Http/Resources/ArkadeDownload.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\ArkadeRelease as ArkadeReleaseResource;
+use App\Http\Resources\ArkadeDownloader as ArkadeDownloaderResource;
+use App\Http\Resources\Organization as OrganizationResource;
+
+class ArkadeDownload extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'nedlastingstidspunkt' => $this->downloaded_at->format("d.m.Y H:i"),
+            'arkadeutgivelse' => new ArkadeReleaseResource($this->arkadeRelease),
+            'arkadenedlaster' => new ArkadeDownloaderResource($this->arkadeDownloader),
+            'organisasjon' => new OrganizationResource($this->organization),
+            'links' => [
+                'self' => route('statistics.download', $this->id),
+                'parent' => route('statistics.downloads'),
+            ],
+        ];
+    }
+}
---
+++ app/Http/Resources/ArkadeDownloader.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArkadeDownloader extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'epost' => $this->email,
+            'har_arkade_v1_erfaring' => $this->has_arkade_v1_experience,
+            'onsker_nyheter' => $this->wants_news,
+            'links' => [
+                'self' => route('statistics.downloader', $this->id),
+                'parent' => route('statistics.downloaders'),
+            ],
+        ];
+    }
+}
