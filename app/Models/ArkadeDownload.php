17c12e887e233b9e1d2a203ce334d45b543e4d20
Jørgen Tellnes <jorgen@arkitektum.no>
2023-03-13 20:18:07 +0100

    Generate empty model factories

--- app/Models/ArkadeDownload.php
+++ app/Models/ArkadeDownload.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ArkadeDownload extends Model
 {
+    use HasFactory;
+
     const CREATED_AT = 'downloaded_at';
     const UPDATED_AT = null;
 
