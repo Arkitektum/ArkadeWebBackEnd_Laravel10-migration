7abb44891834e85ff8f2aae52c6e687076684411
Jørgen Tellnes <jorgen@arkitektum.no>
2023-03-29 16:12:16 +0200

    Adjust statistics menu

--- resources/views/statistics/index.blade.php
+++ resources/views/statistics/index.blade.php
@@ -3,20 +3,21 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Arkade-nedlastingsstatistikk') }}
         </h2>
-        <div class="w-full text-right inline-flex"><a href="{{ route('statistics.index') }}">Tilbake til oversikt</a>
+        <div class="pt-2">
+            <a class="p-2 rounded hover:bg-gray-50 text-sm" href="{{ route('dashboard') }}">&#x2190 Til hovedmeny</a>
         </div>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="overflow-hidden">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <ul class="list-none hover:list-disc">
-                        @foreach($links as $linkName => $href)
-                            <li class="list-group-item"><a class="text-blue-600 visited:text-purple-600" href="{{ $href }}">{{ ucfirst($linkName) }}</a></li>
-                        @endforeach
-                    </ul>
-                </div>
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <ul class="list-none hover:list-disc space-y-6">
+                    @foreach($links as $linkName => $href)
+                    <li class="py-4 shadow rounded hover:bg-gray-50">
+                        <a class="px-4" href="{{ $href }}">{{ ucfirst($linkName) }}</a>
+                    </li>
+                    @endforeach
+                </ul>
             </div>
         </div>
     </div>
