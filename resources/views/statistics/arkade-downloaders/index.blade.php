b749a6cf11ee12f8e9b4558add6c631fd4b04b9b
Jørgen Tellnes <jorgen@arkitektum.no>
2023-03-29 11:51:45 +0200

    Adapt statistics list-views

--- resources/views/statistics/arkade-downloaders/index.blade.php
+++ resources/views/statistics/arkade-downloaders/index.blade.php
@@ -1,37 +1,36 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Arkadenedlastere') }}
+            {{ __('Arkadenedlastere') }} ({{ $totalCount }})
         </h2>
-        <div class="w-full text-right inline-flex"><a href="{{ route('statistics.index') }}">Tilbake til oversikt</a></div>
+        <div class="pt-2 text-sm"><a href="{{ route('statistics.index') }}">&#x2190 Tilbake til oversikt</a></div>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="overflow-hidden">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <table class="w-full text-left text-sm font-light">
-                        <thead class="border-b font-medium dark:border-neutral-500">
-                        <tr>
-                            <th scope="col" class="px-6 py-4">E-post</th>
-                            <th scope="col" class="px-6 py-4">Erfaring fra Arkade 1.x</th>
-                            <th scope="col" class="px-6 py-4">Ønsker Arkade-nyheter</th>
-                            <th scope="col" class="px-6 py-4">Nedlastinger</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($downloaders as $downloader)
-                        <tr class="border-b dark:border-neutral-500">
-                            <td class="whitespace-nowrap px-6 py-4 font-medium">{{ $downloader->email }}</td>
-                            <td class="whitespace-nowrap px-6 py-4">{{ $downloader->has_arkade_v1_experience ? 'Ja' : '' }}</td>
-                            <td class="whitespace-nowrap px-6 py-4">{{ $downloader->wants_news ? 'Ja' : '' }}</td>
-                            <td class="whitespace-nowrap px-6 py-4">{{ $downloader->downloads->count() }}</td>
-                        </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                    <div class="h-16"> {{ $downloaders->links() }} </div>
-                </div>
+    <div class="py-6 max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-4 text-gray-900 dark:text-gray-100">
+                <table class="w-full text-left text-sm">
+                    <thead class="border-b dark:border-neutral-500">
+                    <tr>
+                        <th class="p-4">E-post</th>
+                        <th class="p-4">Erfaring fra Arkade 1.x</th>
+                        <th class="p-4">Ønsker Arkade-nyheter</th>
+                        <th class="p-4">Nedlastinger</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($downloaders as $downloader)
+                    <tr class="border-b dark:border-neutral-500">
+                        <td class="p-4">{{ $downloader->email }}</td>
+                        <td class="p-4">{{ $downloader->has_arkade_v1_experience ? 'Ja' : '' }}
+                        </td>
+                        <td class="p-4">{{ $downloader->wants_news ? 'Ja' : '' }}</td>
+                        <td class="p-4">{{ $downloader->downloads->count() }}</td>
+                    </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                <div class="p-4 pt-6">{{ $downloaders->links() }}</div>
             </div>
         </div>
     </div>
