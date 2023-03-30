b6c1323552efe725c1df695f63692881c7a48c7b
Jørgen Tellnes <jorgen@arkitektum.no>
2023-03-30 16:52:18 +0200

    Expand clickable area in dashbord menu list

--- resources/views/statistics/index.blade.php
+++ resources/views/statistics/index.blade.php
@@ -13,10 +13,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <ul class="list-none hover:list-disc space-y-6">
+                <ul class="list-none space-y-4">
                     @foreach($links as $linkName => $href)
-                    <li class="py-4 shadow rounded hover:bg-gray-50">
-                        <a class="px-4" href="{{ $href }}">{{ ucfirst($linkName) }}</a>
+                    <li>
+                        <a class="p-4 block shadow rounded hover:bg-gray-50"
+                           href="{{ $href }}">{{ ucfirst($linkName) }}</a>
                     </li>
                     @endforeach
                 </ul>
