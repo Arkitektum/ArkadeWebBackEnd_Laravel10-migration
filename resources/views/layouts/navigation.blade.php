2670049f966250406af6ee1a06dd9438aca348a5
Jørgen Tellnes <jorgen@arkitektum.no>
2023-03-31 14:46:16 +0200

    Translate profile and login views

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -39,7 +39,7 @@
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
+                            {{ __('Brukerkonto') }}
                         </x-dropdown-link>
 
                         <!-- Authentication -->
@@ -49,7 +49,7 @@
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                                {{ __('Logg ut') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
@@ -91,7 +91,7 @@
 
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
+                    {{ __('Brukerkonto') }}
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
@@ -101,7 +101,7 @@
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                        {{ __('Log Out') }}
+                        {{ __('Logg ut') }}
                     </x-responsive-nav-link>
                 </form>
             </div>
